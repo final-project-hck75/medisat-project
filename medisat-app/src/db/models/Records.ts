83eb5e087a165baefe31d813a095fd1c6d916b85
Muhammad Bagus Pramaskoro <pramaskoro@gmail.com>
2024-11-01 08:36:42 +0700

    Get Record by Doctor id today - Done

--- medisat-app/src/db/models/Records.ts
+++ medisat-app/src/db/models/Records.ts
@@ -1,6 +1,11 @@
 import { ObjectId } from "mongodb";
 import { db } from "../config";
 
+type RecordTypeToday = {
+  doctorId: string,
+  bookDate: string
+}
+
 class RecordsModel {
   static collection() {
     return db.collection("records");
@@ -13,6 +18,30 @@ class RecordsModel {
     // const record = await this.collection().aggregate(pipeline).toArray();
     // console.log("REDOCRS");
     return record;
+  }
+
+  static async getRecordByDoctorIdToday(params:RecordTypeToday){
+    const{doctorId} = params
+
+    const today = new Date()
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); 
+    const date = String(today.getDate()).padStart(2, '0'); 
+    const formattedDate = `${year}/${month}/${date}`;
+    const pipeline=[
+  {
+    $match:
+      {
+        doctorId: new ObjectId(
+          doctorId
+        ),
+        status: "booked",
+        bookDate: formattedDate,
+      },
+  },
+]
+    const records = await this.collection().aggregate(pipeline).toArray()
+    return records
   }
 }
 
